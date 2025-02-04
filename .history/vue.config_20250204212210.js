c722ea5f343dc57ab4b34b2bbcdc4cfd75ddf1d5
lp <lp@123.cn>
2025-02-04 21:37:47 +0800

    修改bug

--- .history/vue.config_20250204212210.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  publicPath: "./",
-  transpileDependencies: true,
-  devServer: {
-    historyApiFallback: true, // 启用 history 模式支持
-  },
-});
